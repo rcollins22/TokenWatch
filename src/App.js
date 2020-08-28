d4f9a0442b303f0101970aec02ff1920b829a77a
Rashad Collins <rashad.collins22@gmail.com>
2020-08-28 16:30:45 -0400

    simple style

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <Chart name="The 100 Watch App" />
+          <h1>The 100 Watch App</h1> 
           <p>
            The crypto Market app Built with React
           </p>
--- src/List.js
+++ src/List.js
@@ -7,9 +7,9 @@ const List = ({data}) => {
     data.forEach((coin => {
 
         if (coin.price_change_percentage_24h > 0){
-            myArr.push(<li className="coin-inst"><span className="symbol">{coin.symbol}</span><span className="change up">{coin.price_change_percentage_24h}%</span></li>)
+            myArr.push(<li className="coin-inst"><img src={coin.image} className="pic"/><span className="symbol">{coin.name}</span><span className="change up">{Math.round((coin.price_change_percentage_24h + Number.EPSILON) * 100) / 100}%</span></li>)
         } else {
-            myArr.push(<li className="coin-inst"><span className="symbol">{coin.symbol}</span><span className="change down">{coin.price_change_percentage_24h}%</span></li>)
+            myArr.push(<li className="coin-inst"><img src={coin.image} className="pic"/><span className="symbol">{coin.name}</span><span className="change down">{Math.round((coin.price_change_percentage_24h + Number.EPSILON) * 100) / 100}%</span></li>)
         }
     }))
     // const myList = (<ul>{myArr}</ul>)
